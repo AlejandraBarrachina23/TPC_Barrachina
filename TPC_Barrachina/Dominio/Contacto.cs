d48d249c7131cb802fa910a75bc163307646c381
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 11:18:24 -0300

    Dominio - Contacto

---
+++ TPC_Barrachina/Dominio/Contacto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Contacto
+    {
+        public int Telefono { get; set; }
+        public int Celular { get; set; }
+        public string Mail { get; set; }
+        public Direccion Direccion { get; set; }
+    }
+}
