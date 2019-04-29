6ae1eff4f10d8c5d8c919175c09100d133265485
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 11:22:40 -0300

    Dominio - CuentaCorriente

---
+++ TPC_Barrachina/Dominio/CuentaCorriente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class CuentaCorriente
+    {
+        public int CodigoCuentaCorriente { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Debe { get; set; }
+        public decimal Haber { get; set; }
+        public decimal Saldo { get; set; }
+        public decimal LimiteCuenta { get; set; }
+    }
+}
