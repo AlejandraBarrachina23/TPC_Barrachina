53496f7e96c3c7538830be5304946fdf90b934f6
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 11:49:56 -0300

    Dominio - Usuario

---
+++ TPC_Barrachina/Dominio/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Usuario
+    {
+        public int CodigoUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Constrasenia { get; set; }
+    }
+}
