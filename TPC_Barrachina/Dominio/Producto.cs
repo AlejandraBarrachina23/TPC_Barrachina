4f433e92f49cb287c34e9af839c952f702946664
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 11:01:29 -0300

    Dominio - Clase Producto

--- TPC_Barrachina/Dominio/Producto.cs
+++ TPC_Barrachina/Dominio/Producto.cs
@@ -8,5 +8,16 @@ namespace Dominio
 {
     class Producto
     {
+        public int CodigoProducto { get; set; }
+        public string Nombre { get; set; }
+        public TipoProducto TipoProducto { get;set;}
+        public int Stock { get; set; }
+        public int StockCritico { get; set; }
+        public float precioCosto { get; set; }
+        public float precioVenta { get; set; }
+        public Proveedor Proveedor { get; set; }
+        public Rubro Rubro { get; set; }
+        public Sucursal Sucursal { get; set; }
+
     }
 }
