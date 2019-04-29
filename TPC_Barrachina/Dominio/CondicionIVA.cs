5266293d3b746e7520ec220d69d6af96408da3ec
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 11:31:05 -0300

    Dominio - CondiciónIVA

---
+++ TPC_Barrachina/Dominio/CondicionIVA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class CondicionIVA
+    {
+        public int CodigoCondicionIVA { get; set; }
+        public string Nombre { get; set; }
+    }
+}
