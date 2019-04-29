c5c190f48586fe078e02cba4bc487423ac2a4e9e
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 16:24:57 -0300

    Dominio - Empleado

---
+++ TPC_Barrachina/Dominio/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Empleado
+    {
+        public int CodigoEmpleado { get; set; }
+        public string Cargo { get; set; }
+        public string Horario { get; set; }
+        public decimal Remuneracion { get; set; }
+        public Persona Tipo { get; set; }
+
+    }
+}
