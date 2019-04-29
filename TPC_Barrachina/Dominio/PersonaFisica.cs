44e8f1a60daafe55352d12af47b13613f3c8b75f
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 11:11:49 -0300

    Dominio - PersonaJuridica

---
+++ TPC_Barrachina/Dominio/PersonaFisica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class PersonaFisica
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Sexo { get; set; }
+        public Contacto Contacto { get; set; }
+    }
+}
---
+++ TPC_Barrachina/Dominio/PersonaJuridica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class PersonaJuridica
+    {
+        public string RazonSocial { get; set; }
+        public string NumeroCUIT { get; set; }
+        public string NombreFantasia { get; set; }
+        public Contacto Contacto { get; set; }
+        public CondicionIVA CondicionIVA { get; set; }
+    }
+}
