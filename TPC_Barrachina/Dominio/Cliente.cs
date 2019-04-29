648996139e08a11644592d56514ee56b54b92917
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-04-29 11:20:30 -0300

    Dominio - Cliente

---
+++ TPC_Barrachina/Dominio/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Cliente
+    {
+        public int CodigoCliente { get; set; }
+        public Persona TipoCliente { get; set; }
+        public CuentaCorriente CuentaCorriente { get; set; }
+    }
+}
