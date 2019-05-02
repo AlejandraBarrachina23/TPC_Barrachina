9527f950b79371aba6c589189c2ecbf9552a8add
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-05-02 20:59:16 -0300

    Formulario - NuevoSector

---
+++ TPC_Barrachina/PresentacionWinForm/NuevoSector.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PresentacionWinForm
+{
+    partial class NuevoSector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lbApellido = new System.Windows.Forms.Label();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtbNombre = new System.Windows.Forms.TextBox();
+            this.pnlBarraPrincipal = new System.Windows.Forms.Panel();
+            this.lbNombrePrograma = new System.Windows.Forms.Label();
+            this.pnlBarraPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(18, 59);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(74, 13);
+            this.label14.TabIndex = 83;
+            this.label14.Text = "Código Sector";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(121, 56);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(173, 20);
+            this.textBox14.TabIndex = 82;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(166, 172);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(97, 29);
+            this.btnCancelar.TabIndex = 80;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(50, 172);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(100, 29);
+            this.btnAceptar.TabIndex = 81;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // lbApellido
+            // 
+            this.lbApellido.AutoSize = true;
+            this.lbApellido.Location = new System.Drawing.Point(18, 109);
+            this.lbApellido.Name = "lbApellido";
+            this.lbApellido.Size = new System.Drawing.Size(63, 13);
+            this.lbApellido.TabIndex = 77;
+            this.lbApellido.Text = "Descripción";
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(18, 83);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(44, 13);
+            this.lbNombre.TabIndex = 79;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(121, 109);
+            this.txtApellido.Multiline = true;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(173, 40);
+            this.txtApellido.TabIndex = 75;
+            // 
+            // txtbNombre
+            // 
+            this.txtbNombre.Location = new System.Drawing.Point(121, 80);
+            this.txtbNombre.Name = "txtbNombre";
+            this.txtbNombre.Size = new System.Drawing.Size(173, 20);
+            this.txtbNombre.TabIndex = 76;
+            // 
+            // pnlBarraPrincipal
+            // 
+            this.pnlBarraPrincipal.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlBarraPrincipal.Controls.Add(this.lbNombrePrograma);
+            this.pnlBarraPrincipal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlBarraPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.pnlBarraPrincipal.Name = "pnlBarraPrincipal";
+            this.pnlBarraPrincipal.Size = new System.Drawing.Size(365, 41);
+            this.pnlBarraPrincipal.TabIndex = 74;
+            // 
+            // lbNombrePrograma
+            // 
+            this.lbNombrePrograma.AutoSize = true;
+            this.lbNombrePrograma.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombrePrograma.Location = new System.Drawing.Point(122, 16);
+            this.lbNombrePrograma.Name = "lbNombrePrograma";
+            this.lbNombrePrograma.Size = new System.Drawing.Size(104, 13);
+            this.lbNombrePrograma.TabIndex = 9;
+            this.lbNombrePrograma.Text = "NUEVO SECTOR";
+            // 
+            // NuevoSector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 217);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.lbApellido);
+            this.Controls.Add(this.lbNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtbNombre);
+            this.Controls.Add(this.pnlBarraPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NuevoSector";
+            this.Text = "NuevoSector";
+            this.pnlBarraPrincipal.ResumeLayout(false);
+            this.pnlBarraPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lbApellido;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtbNombre;
+        private System.Windows.Forms.Panel pnlBarraPrincipal;
+        private System.Windows.Forms.Label lbNombrePrograma;
+    }
+}
