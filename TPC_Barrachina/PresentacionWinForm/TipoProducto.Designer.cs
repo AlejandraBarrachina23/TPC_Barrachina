f46cb4d7181c82719e320677d4b72a807ba8e7cd
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-05-11 19:23:04 -0300

    WinForm-TipoProducto-Interfaz

--- TPC_Barrachina/PresentacionWinForm/TipoProducto.Designer.cs
+++ TPC_Barrachina/PresentacionWinForm/TipoProducto.Designer.cs
@@ -28,74 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TipoProducto));
             this.lblCodigoTipo = new System.Windows.Forms.Label();
             this.tboxCodigoTipo = new System.Windows.Forms.TextBox();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnAceptar = new System.Windows.Forms.Button();
             this.lblDescripcion = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
             this.tboxDescripcion = new System.Windows.Forms.TextBox();
             this.tboxNombre = new System.Windows.Forms.TextBox();
             this.pnlBarraPrincipal = new System.Windows.Forms.Panel();
-            this.lbNombrePrograma = new System.Windows.Forms.Label();
+            this.lblTipoProducto = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.pnlBarraPrincipal.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblCodigoTipo
             // 
             this.lblCodigoTipo.AutoSize = true;
-            this.lblCodigoTipo.Location = new System.Drawing.Point(42, 60);
+            this.lblCodigoTipo.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoTipo.Location = new System.Drawing.Point(42, 57);
             this.lblCodigoTipo.Name = "lblCodigoTipo";
-            this.lblCodigoTipo.Size = new System.Drawing.Size(64, 13);
+            this.lblCodigoTipo.Size = new System.Drawing.Size(65, 17);
             this.lblCodigoTipo.TabIndex = 72;
             this.lblCodigoTipo.Text = "Código Tipo";
             // 
             // tboxCodigoTipo
             // 
-            this.tboxCodigoTipo.Location = new System.Drawing.Point(140, 57);
+            this.tboxCodigoTipo.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxCodigoTipo.Location = new System.Drawing.Point(140, 54);
             this.tboxCodigoTipo.Name = "tboxCodigoTipo";
-            this.tboxCodigoTipo.Size = new System.Drawing.Size(173, 20);
+            this.tboxCodigoTipo.Size = new System.Drawing.Size(173, 25);
             this.tboxCodigoTipo.TabIndex = 71;
-            // 
-            // btnCancelar
-            // 
-            this.btnCancelar.Location = new System.Drawing.Point(200, 164);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(97, 29);
-            this.btnCancelar.TabIndex = 69;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
-            // 
-            // btnAceptar
-            // 
-            this.btnAceptar.Location = new System.Drawing.Point(84, 164);
-            this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(100, 29);
-            this.btnAceptar.TabIndex = 70;
-            this.btnAceptar.Text = "Aceptar";
-            this.btnAceptar.UseVisualStyleBackColor = true;
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(42, 111);
+            this.lblDescripcion.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(42, 116);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 17);
             this.lblDescripcion.TabIndex = 67;
             this.lblDescripcion.Text = "Descripción";
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(42, 84);
+            this.lblNombre.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(42, 86);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(45, 17);
             this.lblNombre.TabIndex = 68;
             this.lblNombre.Text = "Nombre";
             // 
             // tboxDescripcion
             // 
-            this.tboxDescripcion.Location = new System.Drawing.Point(140, 107);
+            this.tboxDescripcion.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxDescripcion.Location = new System.Drawing.Point(140, 112);
             this.tboxDescripcion.Multiline = true;
             this.tboxDescripcion.Name = "tboxDescripcion";
             this.tboxDescripcion.Size = new System.Drawing.Size(173, 40);
@@ -103,47 +91,83 @@
             // 
             // tboxNombre
             // 
-            this.tboxNombre.Location = new System.Drawing.Point(140, 81);
+            this.tboxNombre.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxNombre.Location = new System.Drawing.Point(140, 83);
             this.tboxNombre.Name = "tboxNombre";
-            this.tboxNombre.Size = new System.Drawing.Size(173, 20);
+            this.tboxNombre.Size = new System.Drawing.Size(173, 25);
             this.tboxNombre.TabIndex = 66;
             // 
             // pnlBarraPrincipal
             // 
-            this.pnlBarraPrincipal.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.pnlBarraPrincipal.Controls.Add(this.lbNombrePrograma);
+            this.pnlBarraPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(115)))), ((int)(((byte)(149)))));
+            this.pnlBarraPrincipal.Controls.Add(this.lblTipoProducto);
             this.pnlBarraPrincipal.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlBarraPrincipal.Location = new System.Drawing.Point(0, 0);
             this.pnlBarraPrincipal.Name = "pnlBarraPrincipal";
             this.pnlBarraPrincipal.Size = new System.Drawing.Size(365, 41);
             this.pnlBarraPrincipal.TabIndex = 64;
             // 
-            // lbNombrePrograma
+            // lblTipoProducto
             // 
-            this.lbNombrePrograma.AutoSize = true;
-            this.lbNombrePrograma.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNombrePrograma.Location = new System.Drawing.Point(108, 15);
-            this.lbNombrePrograma.Name = "lbNombrePrograma";
-            this.lbNombrePrograma.Size = new System.Drawing.Size(156, 13);
-            this.lbNombrePrograma.TabIndex = 9;
-            this.lbNombrePrograma.Text = "NUEVO TIPO PRODUCTO";
+            this.lblTipoProducto.AutoSize = true;
+            this.lblTipoProducto.Font = new System.Drawing.Font("Bebas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoProducto.ForeColor = System.Drawing.Color.White;
+            this.lblTipoProducto.Location = new System.Drawing.Point(136, 9);
+            this.lblTipoProducto.Name = "lblTipoProducto";
+            this.lblTipoProducto.Size = new System.Drawing.Size(104, 23);
+            this.lblTipoProducto.TabIndex = 10;
+            this.lblTipoProducto.Text = "Tipo Producto";
             // 
-            // NuevoTipoProducto
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(115)))), ((int)(((byte)(149)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Bebas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(90, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 37);
+            this.button2.TabIndex = 74;
+            this.button2.Text = "Aceptar";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(115)))), ((int)(((byte)(149)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bebas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(201, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 37);
+            this.button1.TabIndex = 73;
+            this.button1.Text = "Cancelar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // TipoProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(365, 217);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.lblCodigoTipo);
             this.Controls.Add(this.tboxCodigoTipo);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.lblDescripcion);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.tboxDescripcion);
             this.Controls.Add(this.tboxNombre);
             this.Controls.Add(this.pnlBarraPrincipal);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "NuevoTipoProducto";
+            this.Name = "TipoProducto";
             this.Text = "NuevoTipoProducto";
             this.pnlBarraPrincipal.ResumeLayout(false);
             this.pnlBarraPrincipal.PerformLayout();
@@ -156,13 +180,13 @@
 
         private System.Windows.Forms.Label lblCodigoTipo;
         private System.Windows.Forms.TextBox tboxCodigoTipo;
-        private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Label lblDescripcion;
         private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.TextBox tboxDescripcion;
         private System.Windows.Forms.TextBox tboxNombre;
         private System.Windows.Forms.Panel pnlBarraPrincipal;
-        private System.Windows.Forms.Label lbNombrePrograma;
+        private System.Windows.Forms.Label lblTipoProducto;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
