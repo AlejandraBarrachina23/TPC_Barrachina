52b0120c5fb3a3a9655f0261eca88d049e70f05b
Alejandra Barrachina <alejandrabarrachina23@gmail.com>
2019-05-11 20:21:21 -0300

    WinForm - Descuento - Interfaz

--- TPC_Barrachina/PresentacionWinForm/Descuento.Designer.cs
+++ TPC_Barrachina/PresentacionWinForm/Descuento.Designer.cs
@@ -28,76 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Descuento));
             this.lblCodigoDescuento = new System.Windows.Forms.Label();
             this.tboxCodigoDescuento = new System.Windows.Forms.TextBox();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnAceptar = new System.Windows.Forms.Button();
             this.lblDescripcion = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
             this.tboxDescripcion = new System.Windows.Forms.TextBox();
             this.txtbNombre = new System.Windows.Forms.TextBox();
             this.pnlBarraPrincipal = new System.Windows.Forms.Panel();
-            this.lbNombrePrograma = new System.Windows.Forms.Label();
             this.tboxPorcentaje = new System.Windows.Forms.TextBox();
             this.lblPorcentaje = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
             this.pnlBarraPrincipal.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblCodigoDescuento
             // 
             this.lblCodigoDescuento.AutoSize = true;
-            this.lblCodigoDescuento.Location = new System.Drawing.Point(22, 60);
+            this.lblCodigoDescuento.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoDescuento.Location = new System.Drawing.Point(22, 53);
             this.lblCodigoDescuento.Name = "lblCodigoDescuento";
-            this.lblCodigoDescuento.Size = new System.Drawing.Size(95, 13);
+            this.lblCodigoDescuento.Size = new System.Drawing.Size(94, 17);
             this.lblCodigoDescuento.TabIndex = 72;
             this.lblCodigoDescuento.Text = "Código Descuento";
             // 
             // tboxCodigoDescuento
             // 
-            this.tboxCodigoDescuento.Location = new System.Drawing.Point(125, 57);
+            this.tboxCodigoDescuento.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxCodigoDescuento.Location = new System.Drawing.Point(125, 50);
             this.tboxCodigoDescuento.Name = "tboxCodigoDescuento";
-            this.tboxCodigoDescuento.Size = new System.Drawing.Size(173, 20);
+            this.tboxCodigoDescuento.Size = new System.Drawing.Size(173, 25);
             this.tboxCodigoDescuento.TabIndex = 71;
-            // 
-            // btnCancelar
-            // 
-            this.btnCancelar.Location = new System.Drawing.Point(181, 191);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(97, 29);
-            this.btnCancelar.TabIndex = 69;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
-            // 
-            // btnAceptar
-            // 
-            this.btnAceptar.Location = new System.Drawing.Point(65, 191);
-            this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(100, 29);
-            this.btnAceptar.TabIndex = 70;
-            this.btnAceptar.Text = "Aceptar";
-            this.btnAceptar.UseVisualStyleBackColor = true;
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(22, 132);
+            this.lblDescripcion.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(22, 136);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 17);
             this.lblDescripcion.TabIndex = 67;
             this.lblDescripcion.Text = "Descripción";
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(22, 84);
+            this.lblNombre.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(22, 81);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(45, 17);
             this.lblNombre.TabIndex = 68;
             this.lblNombre.Text = "Nombre";
             // 
             // tboxDescripcion
             // 
-            this.tboxDescripcion.Location = new System.Drawing.Point(125, 132);
+            this.tboxDescripcion.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxDescripcion.Location = new System.Drawing.Point(125, 136);
             this.tboxDescripcion.Multiline = true;
             this.tboxDescripcion.Name = "tboxDescripcion";
             this.tboxDescripcion.Size = new System.Drawing.Size(173, 40);
@@ -105,57 +93,95 @@
             // 
             // txtbNombre
             // 
-            this.txtbNombre.Location = new System.Drawing.Point(125, 81);
+            this.txtbNombre.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbNombre.Location = new System.Drawing.Point(125, 78);
             this.txtbNombre.Name = "txtbNombre";
-            this.txtbNombre.Size = new System.Drawing.Size(173, 20);
+            this.txtbNombre.Size = new System.Drawing.Size(173, 25);
             this.txtbNombre.TabIndex = 66;
             // 
             // pnlBarraPrincipal
             // 
-            this.pnlBarraPrincipal.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.pnlBarraPrincipal.Controls.Add(this.lbNombrePrograma);
+            this.pnlBarraPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(115)))), ((int)(((byte)(149)))));
+            this.pnlBarraPrincipal.Controls.Add(this.lblDescuento);
             this.pnlBarraPrincipal.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlBarraPrincipal.Location = new System.Drawing.Point(0, 0);
             this.pnlBarraPrincipal.Name = "pnlBarraPrincipal";
             this.pnlBarraPrincipal.Size = new System.Drawing.Size(354, 41);
             this.pnlBarraPrincipal.TabIndex = 64;
             // 
-            // lbNombrePrograma
-            // 
-            this.lbNombrePrograma.AutoSize = true;
-            this.lbNombrePrograma.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNombrePrograma.Location = new System.Drawing.Point(110, 15);
-            this.lbNombrePrograma.Name = "lbNombrePrograma";
-            this.lbNombrePrograma.Size = new System.Drawing.Size(130, 13);
-            this.lbNombrePrograma.TabIndex = 9;
-            this.lbNombrePrograma.Text = "NUEVO DESCUENTO";
-            // 
             // tboxPorcentaje
             // 
+            this.tboxPorcentaje.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tboxPorcentaje.Location = new System.Drawing.Point(125, 107);
             this.tboxPorcentaje.Name = "tboxPorcentaje";
-            this.tboxPorcentaje.Size = new System.Drawing.Size(173, 20);
+            this.tboxPorcentaje.Size = new System.Drawing.Size(173, 25);
             this.tboxPorcentaje.TabIndex = 73;
             // 
             // lblPorcentaje
             // 
             this.lblPorcentaje.AutoSize = true;
+            this.lblPorcentaje.Font = new System.Drawing.Font("Oswald Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPorcentaje.Location = new System.Drawing.Point(22, 110);
             this.lblPorcentaje.Name = "lblPorcentaje";
-            this.lblPorcentaje.Size = new System.Drawing.Size(58, 13);
+            this.lblPorcentaje.Size = new System.Drawing.Size(58, 17);
             this.lblPorcentaje.TabIndex = 68;
             this.lblPorcentaje.Text = "Porcentaje";
             // 
-            // NuevoDescuento
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Font = new System.Drawing.Font("Bebas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescuento.ForeColor = System.Drawing.Color.White;
+            this.lblDescuento.Location = new System.Drawing.Point(142, 9);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(80, 23);
+            this.lblDescuento.TabIndex = 74;
+            this.lblDescuento.Text = "DESCUENTO";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(115)))), ((int)(((byte)(149)))));
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Bebas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(192, 185);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(94, 37);
+            this.btnCancelar.TabIndex = 74;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(115)))), ((int)(((byte)(149)))));
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Bebas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Image = ((System.Drawing.Image)(resources.GetObject("btnAceptar.Image")));
+            this.btnAceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAceptar.Location = new System.Drawing.Point(81, 185);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(94, 37);
+            this.btnAceptar.TabIndex = 75;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            // 
+            // Descuento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(354, 232);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.tboxPorcentaje);
             this.Controls.Add(this.lblCodigoDescuento);
             this.Controls.Add(this.tboxCodigoDescuento);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.lblDescripcion);
             this.Controls.Add(this.lblPorcentaje);
             this.Controls.Add(this.lblNombre);
@@ -163,7 +189,7 @@
             this.Controls.Add(this.txtbNombre);
             this.Controls.Add(this.pnlBarraPrincipal);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "NuevoDescuento";
+            this.Name = "Descuento";
             this.Text = "TipoDescuento";
             this.pnlBarraPrincipal.ResumeLayout(false);
             this.pnlBarraPrincipal.PerformLayout();
@@ -176,15 +202,15 @@
 
         private System.Windows.Forms.Label lblCodigoDescuento;
         private System.Windows.Forms.TextBox tboxCodigoDescuento;
-        private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Label lblDescripcion;
         private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.TextBox tboxDescripcion;
         private System.Windows.Forms.TextBox txtbNombre;
         private System.Windows.Forms.Panel pnlBarraPrincipal;
-        private System.Windows.Forms.Label lbNombrePrograma;
         private System.Windows.Forms.TextBox tboxPorcentaje;
         private System.Windows.Forms.Label lblPorcentaje;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
     }
 }
